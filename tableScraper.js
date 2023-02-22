d9aaab3a8deff62a790f6266142658cde38650b8
Jamal West <thejamalwest@gmail.com>
2023-02-21 19:23:16 -0600

    Develop (#2)
    * gitignore commit
    
    * package.json
    
    * axios cheerio & puppeteer npm installs
    
    * updated packages: rm puppeteer, add obj-to-csv
    
    * updated gitignore
    
    * axios & cheerio impl
    
    * added getChart functionality to save chart img src
    
    * image-downloader npm
    
    * added saveChartImgs function
    
    * proper removal of objects-to-csv

--- tableScraper.js
+++ tableScraper.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const ObjectsToCsv = require('objects-to-csv');
+const download = require('image-downloader');
+
+const DIR = "/Users/jamalwest/Desktop/finvizScreener Results/2-21";
 
 (async function getTickers() {
   try {
@@ -8,13 +10,12 @@ const ObjectsToCsv = require('objects-to-csv');
     const response = await axios('https://finviz.com/screener.ashx?v=111&f=cap_microover,sh_avgvol_o1000,sh_curvol_o2000,sh_price_o10,sh_relvol_o1.5,ta_averagetruerange_o1,ta_change_u3&o=price');
     // await html document
     const html = await response.data;
-    
-    console.log('Connection Successful.');
+
+    console.log('Ticker Connection Successful...');
 
     // parse using cheerio
     const $ = cheerio.load(html);
     const allRows = $('a.screener-link-primary');
-    
     console.log(`${allRows.length} tickers found, parsing now`);
 
     // create tickers array for return
@@ -31,3 +32,55 @@ const ObjectsToCsv = require('objects-to-csv');
     console.log(error);
   }
 })();
+
+
+(async function getChartUrls() {
+  try {
+    // make axios call to finviz screener (chart view)
+    const response = await axios.get('https://finviz.com/screener.ashx?v=211&f=cap_microover,sh_avgvol_o1000,sh_curvol_o2000,sh_price_o10,sh_relvol_o1.5,ta_averagetruerange_o1,ta_change_u3&o=price');
+    // await html document
+    const html = await response.data;
+
+    console.log('Chart Connection Successful...');
+
+    // parse using cheerio
+    const $ = cheerio.load(html);
+    const allCharts = $('img.border-white');
+
+    console.log(`${allCharts.length} charts found, saving imgs now`);
+
+    const imgUrlArray = [];
+
+    allCharts.each((idx, chart) => {
+      const chartUrl = $(chart).attr('src');
+      imgUrlArray.push(chartUrl);
+    });
+
+    // save chart images to DIR directory of choice
+    saveChartImgs(imgUrlArray, DIR);
+
+    return imgUrlArray;
+
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+function saveChartImgs(imgUrls, dir) {
+
+  for (let idx = 0; idx < imgUrls.length; idx++) {
+    const Url = imgUrls[idx];
+    const options = {
+      url: `${Url}`,
+      dest: `${dir}/chart_${idx}.jpg`, // will be saved to /path/to/dest/chart_#.jpg
+    };
+
+    download.image(options)
+      .then(({
+        filename
+      }) => {
+        console.log('Saved to', filename); // saved to /path/to/dest/chart_#.jpg
+      })
+      .catch((err) => console.error(err));
+  }
+}
